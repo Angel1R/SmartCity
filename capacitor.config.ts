a87e7acb5b8733428076fe25fa698719b731e188
Ian Rodriguez <rodriguez.ian.6120@gmial.com>
2025-11-19 09:09:42 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ProyectoSmart',
+  webDir: 'www'
+};
+
+export default config;
